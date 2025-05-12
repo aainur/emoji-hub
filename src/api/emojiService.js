a4c853398c94cdd93db5db28e8f30151a887e723
aainur <ainur@Ainurs-MacBook-Pro.local>
2025-05-13 02:57:29 +0500

    Fixed Vercel API route

--- src/api/emojiService.js
+++ src/api/emojiService.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export const fetchEmojis = async () => {
   try {
     const response = await axios.get("/api/emojis");
+    //const response = await axios.get("https://emoji-hub.vercel.app/api/emojis");
     return response.data;
   } catch (error) {
     console.error("Error fetching emojis:", error);
