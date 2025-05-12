d46c8851a097a161f4282bdb379b120014be7385
aainur <ainur@Ainurs-MacBook-Pro.local>
2025-05-13 02:13:21 +0500

    With Vercel Deployment

--- src/services/server.js
+++ src/services/server.js
@@ -1,14 +1,12 @@
-console.log("Starting server...");
-
 require("dotenv").config();
+console.log("LOCAL environment variable:", process.env.LOCAL);  // Проверь, что выводится
+console.log("PORT environment variable:", process.env.PORT); 
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 
 const app = express();
 app.use(cors());
-const PORT = 5000;
-//const PORT = process.env.PORT || 5000;
 
 app.get("/api/emojis", async (req, res) => {
   try {
@@ -21,14 +19,32 @@ app.get("/api/emojis", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-}).on("error", (error) => {
-  if (error.code === "EADDRINUSE") {
-    console.error(`Port ${PORT} is already in use. Try a different port or stop the process using it.`);
-  } else {
-    console.error("Server error:", error.message);
-  }
-  process.exit(1);
-});
+if (process.env.LOCAL === "true") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
